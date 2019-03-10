86c1184e4ea08882165ff85517a648a6e862ce8d
LukeDeBeneducci <beneducciluke@gmail.com>
2019-03-10 15:17:14 +0000

    single stream working

--- server.js
+++ server.js
@@ -6,25 +6,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var connections = {};
+
 io.on('connection', (socket) =>{
-    console.log('a user connected');
-
-    socket.on('offer',  (data) => {
-        console.log(data)
-        console.log('relaying offer');
-        socket.broadcast.emit('offer', data);
-      });
     
-      socket.on('answer', (data) => {
-        console.log('relaying answer');
-        socket.broadcast.emit('answer', data);
-      });
-    
-      socket.on('candidate',  (data) => {
-        console.log('relaying candidate');
-        socket.broadcast.emit('candidate', data);
-      });
-
+  socket.on('message', (message) => {
+    if(message.sender === 'web'){
+		if(message.type === 'connected'){
+			console.log('web-connected')
+		} else {
+			console.log('Client said: ', message)
+			socket.broadcast.emit('message-for-mobile', message)
+		}	
+    } else if (message.sender === 'mobile'){
+		if(message.type === 'connected'){
+			console.log('mobile-connected')
+		} else {
+			console.log('Client said: ', message)
+			socket.broadcast.emit('message-for-web', message)
+		}
+    } 
+  })
 });
 
 http.listen(6500, () => {
