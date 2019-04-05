d50654be604954bd0091311d61da74d0f5fcef37
LukeDeBeneducci <beneducciluke@gmail.com>
2019-04-05 11:15:32 +0100

    removed logging

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ io.on('connection', (socket) =>{
 
 	socket.on('connected', (message) => {
 		if(message.sender === 'web'){
-			console.log('web-connected')
 			webConnection = socket.id;
 		}
 	});
@@ -31,49 +30,35 @@ io.on('connection', (socket) =>{
 		} 
 	})
 	socket.on('disconnect', () => {
-		console.log('socket closed', socket.id)
 		delete connections[socket.id]
 		io.to(webConnection).emit('removed-connection', socket.id, connections)
 	})
     
 	socket.on('message', (toID, message) => {
 		if(message.sender === 'web'){
-			console.log('Web said: ', message)
-			console.log('toMobID:', toID)
 			io.to(toID).emit('message', message)
 		} else if (message.sender === 'mobile'){
-			console.log('Mobile said: ', message)
-			console.log('toWebID: ', webConnection)
 			io.to(webConnection).emit('message', socket.id, message)
 		} 
 	})
 
 	socket.on('text-message', (toID, message) => {
-		console.log('Web said: ', message)
-		console.log('toMobID:', toID)
 		io.to(toID).emit('text-message', message)
 	})
 
 	socket.on('air', (toID, message) => {
-		console.log('Web said: ', message)
-		console.log('toMobID:', toID)
 		io.to(toID).emit('air', message)
 	})
 
 	socket.on('options-message', (toID, otherIDs, message) => {
-		console.log('Web said: ', message)
-		console.log('toMobID:', toID)
 		io.to(toID).emit('options-message',toID, otherIDs, message)
 	})
 
 	socket.on('option-taken', (toID, otherIDs, message)=>{
-		console.log('Mob said: ', message)
-		console.log('toMobID', toID)
 		io.to(toID).emit('option-taken', socket.id, otherIDs, message)
 	})
 
 	socket.on('options-response', (message) => {
-		console.log('Mobile said: ', message)
 		io.to(webConnection).emit('options-response', connections[socket.id], message)
 	})
 
